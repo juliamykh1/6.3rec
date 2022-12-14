f245e9369f101dd28c7c3c8369283d93ae9d2766
Julia <mihajloviculia65@gmail.com>
2022-12-14 22:46:43 +0200

    first master commit

---
+++ 6.3rec/6.3rec.cpp
@@ -0,0 +1,87 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<time.h>
+
+using namespace std;
+
+template <typename T>
+void Create1(T* mas, const int n, int i)
+{
+	mas[i] = (T)(rand() % 6);
+	if (i < n - 1)
+	   Create1(mas, n, i + 1);
+}
+
+void Create2(int* mas, const int n, int i)
+{
+	mas[i] = (rand() % 6);
+	if (i < n - 1)
+		Create2(mas, n, i + 1);
+}
+
+template <typename T>
+void Print1(T* mas, const int n, int i)
+{
+	cout << mas[i] << " ";
+	if (i < n - 1)
+		Print1(mas, n, i + 1);
+}
+
+void Print2(int* mas, const int n, int i)
+{
+	cout << mas[i] << " ";
+	if (i < n - 1)
+	    Print2(mas, n, i + 1);
+}
+
+template <typename T>
+int Perev1(T* mas, const int n, int i)                    //шаблонна функція             
+{
+	if (n < 2)
+		cerr << "in arrays one element" << endl;
+	if (i < n && !(mas[i - 1] < mas[i]))
+		return Perev1<T>(mas, n, i + 1);
+	else
+		return i >= n;
+}
+
+int Perev2(int* mas, const int n, int i)            // звичайна функція
+{
+	if (n < 2)
+		cerr << "in arrays one element" << endl;
+	if (i < n && !(mas[i - 1] < mas[i]))
+		return Perev2(mas, n, i + 1);
+	else
+		return i >= n;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int k = 5;
+	int a[k];
+
+	Create1<int>(a, k, 0);
+	Create2(a, k, 0);
+
+	cout << "a[ ";
+	Print1<int>(a, k, 0);
+	cout << "]" << endl;
+
+	cout << "a[ ";
+	Print2(a, k, 0);
+	cout << "]" << endl;
+
+	if (Perev1<int>(a, k, 1) == 1)  
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+
+	if (Perev2(a, k, 1) == 1)
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+
+	return 0;
+}
---
+++ UnitTest6.3rec/UnitTest6.3rec.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../6.3rec/6.3rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63rec
+{
+	TEST_CLASS(UnitTest63rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int k = 3;
+			int a[k] = { 3,1,2 };
+			t = Perev1(a, k, 1);
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
